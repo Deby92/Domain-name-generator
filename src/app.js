757d529fa2615fc91e10f4c2c229f8875fe2fdac
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-24 21:07:39 +0000

    viernes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our", "i", "you", "he", "it", "we", "they"];
+let adj = ["great", "sweet", "hot", "cold", "warm", "dry", "ligth", "clean"];
+let noun = [
+  "book",
+  "baloon",
+  "chair",
+  "popcorn",
+  "falafel",
+  "camera",
+  "lady",
+  "library",
+  "puppy",
+  "fly"
+];
+let suffix = [".com", ".co", ".es", ".cl", ".au", ".nz", ".ar", ".hu"];
+con;
