ec432e8934d4b750db314e4ed75433ef3fb0adb0
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-24 21:36:06 +0000

    viernes

--- src/app.js
+++ src/app.js
@@ -5,19 +5,31 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ["the", "our", "i", "you", "he", "it", "we", "they"];
-let adj = ["great", "sweet", "hot", "cold", "warm", "dry", "ligth", "clean"];
-let noun = [
-  "book",
-  "baloon",
-  "chair",
-  "popcorn",
-  "falafel",
-  "camera",
-  "lady",
-  "library",
-  "puppy",
-  "fly"
-];
-let suffix = [".com", ".co", ".es", ".cl", ".au", ".nz", ".ar", ".hu"];
-con;
+window.onload = () => {
+  //write your code here
+  document.querySelector("#Random-domain").innerHTML = generateDomain();
+  document.getElementById('Generate').addEventListener("click", set.random-button)
+  console.log("Hello World");
+};
+
+const pronoun = ["The", "Our", "I", "You", "He", "It", "We", "They"];
+const adjetive = ["great", "sweet", "hot", "cold", "warm", "dry", "ligth", "clean"];
+const noun = ["book", "baloon", "chair", "popcorn", "falafel", "camera", "lady", "library", "puppy", "fly"];
+const suffix = [".com", ".co", ".es", ".cl", ".au", ".nz", ".ar", ".hu"];
+
+let generateDomain = () => {
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjetiveIndex = Math.floor(Math.random() * adjetive.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let suffixIndex = Math.floor(Math.random() * suffix.length);
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    adjetive[adjetiveIndex] +
+    " " +
+    noun[nounIndex] +
+    " " +
+    suffix[suffixIndex] +
+    
+  );};
+domain(pronoun, adjetive, noun, suffix);
