a0f1e904c8db553e6417b54347a001cc4877560f
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-29 23:49:03 +0000

    miercoles

--- src/app.js
+++ src/app.js
@@ -7,29 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   //write your code here
-  document.querySelector("#Random-domain").innerHTML = generateDomain();
-  document.getElementById('Generate').addEventListener("click", set.random-button)
-  console.log("Hello World");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = generateDomain();
+  });
+  //write your code here
+  document.querySelector("#domain").innerHTML = generateDomain();
+  console.log("hello World");
 };
 
-const pronoun = ["The", "Our", "I", "You", "He", "It", "We", "They"];
-const adjetive = ["great", "sweet", "hot", "cold", "warm", "dry", "ligth", "clean"];
-const noun = ["book", "baloon", "chair", "popcorn", "falafel", "camera", "lady", "library", "puppy", "fly"];
-const suffix = [".com", ".co", ".es", ".cl", ".au", ".nz", ".ar", ".hu"];
+const generateDomain = () => {
 
-let generateDomain = () => {
-  let pronounIndex = Math.floor(Math.random() * pronoun.length);
-  let adjetiveIndex = Math.floor(Math.random() * adjetive.length);
-  let nounIndex = Math.floor(Math.random() * noun.length);
-  let suffixIndex = Math.floor(Math.random() * suffix.length);
-  return (
-    pronoun[pronounIndex] +
-    " " +
-    adjetive[adjetiveIndex] +
-    " " +
-    noun[nounIndex] +
-    " " +
-    suffix[suffixIndex] +
-    
-  );};
-domain(pronoun, adjetive, noun, suffix);
+  var pronoun = ["the", "our", "your", "her"];
+  var adj = ["great", "big", "sweet", "hot", "ligth", "dirty"];
+  var noun = ["jogger", "racoon", "puppy", "chair", "camera", "fly", "popcorn"];
+  let suffix = [".com", ".ca", "nz", ".es", ".cl", ".ar"];
+
+  generateDomain[0]
+    for(let i = 0; i < generateDomain.length; i++){
+      console.log(generateDomain[i]); ["the", "our", "your", "her"];
+      for
+    }
+
+
+/*   var pronoun = ["the", "our", "your", "her"];
+  var adj = ["great", "big", "sweet", "hot", "ligth", "dirty"];
+  var noun = ["jogger", "racoon", "puppy", "chair", "camera", "fly", "popcorn"];
+  let suffix = [".com", ".ca", "nz", ".es", ".cl", ".ar"];
+
+  let domain = (pronoun, adj, noun) => {
+    for (var p = 0; i < pronoun.length; p++) {
+      for (var a = 0; i < adj.length; a++) {
+        for (var n = 0; i < noun.length; n++)
+          for (x = 0; i < suffix.length; x++) {
+            console.log(pronoun[p] + adj[a] + noun[n] + suffix[x]);
+          }
+      }
+    }
+  };
+};
+domain(pronoun, adj, noun); */
